dd7054df9e793e6e15ec6e524ac85743770b3584
soer145a <soer145a@stud.kea.dk>
2018-06-07 12:23:24 +0100

    links

--- javascript.js
+++ javascript.js
@@ -1,4 +1,9 @@
-$("#link_knap").click(function () {
+$("#link_knap1").click(function () {
+	window.location = $(this).find("a").attr("href");
+	return false;
+});
+
+$("#link_knap2").click(function () {
 	window.location = $(this).find("a").attr("href");
 	return false;
 });
