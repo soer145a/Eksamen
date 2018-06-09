1e25b8413a78fc440f908f30ae13d6f76a431ea0
soer145a <soer145a@stud.kea.dk>
2018-06-09 21:49:31 +0100

    forside

--- javascript.js
+++ javascript.js
@@ -1,4 +1,24 @@
+$(window).on("load", normalImg);
+
+
 $(".link_knap").click(function () {
 	window.location = $(this).find("a").attr("href");
 	return false;
 });
+
+function hoverUx(x) {
+	$("#ux_forside").hide();
+	$("#ux_forside_2").show();
+}
+
+function normalImg(x) {
+	$("#ani_forside").show();
+	$("#ani_forside1").hide();
+	$("#ux_forside").show();
+	$("#ux_forside_2").hide();
+}
+
+function hoverAni(x) {
+	$("#ani_forside").hide();
+	$("#ani_forside1").show();
+}
