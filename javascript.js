31119ce908611c7f9f8b5f17e22db1db4490ff86
soer145a <soer145a@stud.kea.dk>
2018-06-09 22:01:24 +0100

    javascript og forside knapper

--- javascript.js
+++ javascript.js
@@ -16,9 +16,23 @@ function normalImg(x) {
 	$("#ani_forside1").hide();
 	$("#ux_forside").show();
 	$("#ux_forside_2").hide();
+	$("#video_forside1").hide();
+	$("#video_forside").show();
+	$("#content_forside1").hide();
+	$("#content_forside").show();
 }
 
 function hoverAni(x) {
 	$("#ani_forside").hide();
 	$("#ani_forside1").show();
+}
+
+function hoverVideo(x) {
+	$("#video_forside").hide();
+	$("#video_forside1").show();
+}
+
+function hoverContent(x) {
+	$("#content_forside").hide();
+	$("#content_forside1").show();
 }
