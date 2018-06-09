70c6e69bfc78bc1887d1bfb4c2e15796e4cf5cfe
soer145a <soer145a@stud.kea.dk>
2018-06-09 19:52:23 +0100

    UX færdig

--- javascript.js
+++ javascript.js
@@ -1,9 +1,4 @@
-$("#link_knap1").click(function () {
-	window.location = $(this).find("a").attr("href");
-	return false;
-});
-
-$("#link_knap2").click(function () {
+$(".link_knap").click(function () {
 	window.location = $(this).find("a").attr("href");
 	return false;
 });
